6e4ab8f6705b510f9480ea4a733449972b4c0449
Trinadh Koya <trinadhkoya@Trinadhs-MacBook-Pro.local>
2017-03-15 00:31:06 +0530

    inital commit

---
+++ main_app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+
+# class TreasureForm(forms.Form):
+#     name = forms.CharField(max_length=100)
+#     value = forms.DecimalField(max_digits=10, decimal_places=2)
+#     material = forms.CharField(max_length=100)
+#     location = forms.CharField(max_length=100)
+#     img_url = forms.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.name
+from .models import Treasure
+
+'''
+the above method is little lengthy,if we have another simple strategy that is nothing but meta
+'''
+
+class TreasureForm(forms.ModelForm):
+    class Meta:
+        model=Treasure
+        fields=['name','value','location','img_url','material']
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from .models import Treasure
+from .forms import TreasureForm
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 
 def index(request):
-    treasures=Treasure.objects.all()
-    return render(request, 'index.html', {'treasures': treasures})
+    treasures = Treasure.objects.all()
+    form=TreasureForm()#an empty form to display in home page.we can check conditions later (if logged in or not)
+    return render(request, 'index.html', {'treasures': treasures,'form':form})
+
+
+def detail(request, treasure_id):
+    treasures = Treasure.objects.get(id=treasure_id)
+    return render(request, 'detail.html', {'treasure': treasures})
+
+
+# def post_treasure(request):
+#     form = TreasureForm(request.POST)
+#     if form.is_valid():
+#         treasure = Treasure(name=form.cleaned_data['name'],
+#                             value=form.cleaned_data['value'],
+#                             location=form.cleaned_data['location'],
+#                             material=form.cleaned_data['material'],
+#                             img_url=form.cleaned_data['img_url'])
+#         # don't forget to save
+#         treasure.save()
+#         return HttpResponseRedirect("/")
+'''
+we can do the same stuff in simple steps using
+'''
+
+def post_treasure(request):
+    form=TreasureForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+    return HttpResponseRedirect("/")
